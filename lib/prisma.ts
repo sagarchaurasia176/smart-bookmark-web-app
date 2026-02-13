99f7f8ab3bbdfa89d2bed5b79909e8e1708186fd
sagarchaurasia176 <sagar.volgainfosys@gmail.com>
2026-02-13 13:33:28 +0530

    fix:the prisma client issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from './generated/prisma/client'
+
+
+//Connection string for the postrgres sql with the prisma
 const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
