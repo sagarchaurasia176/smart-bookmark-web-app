e9e2c05701590f0f5ca7906a0f2b68937fd94e09
sagarchaurasia176 <sagar.volgainfosys@gmail.com>
2026-02-13 07:14:37 +0530

    fix:the prisma issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,32 +1,6 @@
-// import { PrismaClient } from "./generated/prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { Pool } from "pg";
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   max: 10, // Maximum number of clients in the pool
-//   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-//   connectionTimeoutMillis: 10000, // Return an error after 10 seconds if connection could not be established
-// });
-// const adapter = new PrismaPg(pool);
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-
-import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "./generated/prisma/client";
-// import { PrismaClient } from '../generated/prisma/client'
-
 const connectionString = `${process.env.DATABASE_URL}`
-
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
-
 export { prisma }
